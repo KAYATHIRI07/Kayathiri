89e95135bcf9600b8a7a5985a2bb7bcc52ab83b4
KAYATHIRI07 <kayathirinagarajan@gmail.com>
2025-01-29 15:26:29 +0530

    portfolio

---
+++ new.js
@@ -0,0 +1,23 @@
+/*let user={
+    name:"Riya",
+    age:25,
+    "is Singer":true,
+}
+console.log(user);
+for(let key in user){
+    console.log(key+":"+user[key]);
+}
+const array=[1,2,3,6,4,5]
+for(let value of array){
+    console.log(value)
+}
+let marks=90; 
+let grade = (marks>=90)? "A" : (marks>70)? "B" :(marks>=40) ? "C" 
+: "F";
+console.log(grade);*/
+let array=["hellp"]
+let arr2=[90,91,92,93,94]
+let arrCopy=[...array,...arr2]
+console.log(arrCopy);
+/*let user = (name:"user1",age:"20")
+let address=(city:"CBE",country:"India")*/
